f1cd6c75af4de9effa128d4f0cac6136d5090ff6
Khang Bui Tran Duy <144929763+khangbkk23@users.noreply.github.com>
2026-03-03 19:15:26 +0700

    Update solution for tanh-activation

--- tanh-activation/tanh-activation.py
+++ tanh-activation/tanh-activation.py
@@ -6,4 +6,4 @@ def tanh(x):
     """
     # Write code here
     x = np.asarray(x, dtype=float)
-    return (np.exp(x) - np.exp(-x)) / (np.exp(x) + np.exp(-x))
+    return (np.exp(x) - np.exp(-x)) / (np.exp(x) + np.exp(-x)).astype(float)
